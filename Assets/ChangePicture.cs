fb3639152410551486bb530eff78a61058480e90
Phil Turnbull <p.turnbull@auckland.ac.nz>
2020-02-25 13:03:29 +1300

    Added support for movies, new logo, splash screen, and versioning

--- Assets/ChangePicture.cs
+++ Assets/ChangePicture.cs
@@ -12,10 +12,9 @@ public class ChangePicture : MonoBehaviour
 {
     public Sprite[] theIms;
     public Image defaultImage;
-    public Button foldPicBut, StartBut;
+    public Button foldPicBut;
     public TMP_Text statusText;
-    private bool imgLoaded, gtg;
-    private GameObject men;
+    public bool imgLoaded;
     public GetGaze gg;
 
     private KeyCode[] keyCodes = {
@@ -36,17 +35,7 @@ public class ChangePicture : MonoBehaviour
     private void Start()
     {
         foldPicBut.onClick.AddListener(LoadImages);
-        StartBut.onClick.AddListener(Begin);
-        men = GameObject.Find("WelcomeMenu");
         statusText.text = "LV Simulator V" + Application.version;
-    }
-
-    private void Begin()
-    {
-        gtg = true;
-        men.SetActive(false);
-        gg.enabled = true;
-        defaultImage.sprite = theIms[UnityEngine.Random.Range(0, theIms.Length)];
     }
 
     private void LoadImages()
@@ -62,6 +51,8 @@ public class ChangePicture : MonoBehaviour
                 theIms[i] = LoadNewSprite(fileInfo[i]);
             }
             imgLoaded = true;
+            statusText.text = theIms.Length + " images loaded";
+
         }
         else 
         { 
@@ -95,8 +86,6 @@ public class ChangePicture : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        StartBut.interactable = imgLoaded;
-
         for (int i = 0; i < keyCodes.Length; i++)
         {
             if (Input.GetKeyUp(keyCodes[i]) && theIms.Length > i) 
---
+++ Assets/LoadMovie.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.Video;
+
+public class LoadMovie : MonoBehaviour
+{
+    public Button movBut;
+    public TMP_Text statusText;
+    public Image theScreen;
+    public GameObject welcomeScreen;
+    public GetGaze gg;
+    public bool movieLoaded = false;
+
+    void Start()
+    {
+        movBut.onClick.AddListener(LoadMovies);
+    }
+
+    private void LoadMovies()
+    {
+        string movFile = EditorUtility.OpenFilePanel("Select Movie File", Application.dataPath, "mov");
+        if (movFile != "")
+        {           
+            theScreen.GetComponent<VideoPlayer>().url = movFile;
+            movieLoaded = true;
+            statusText.text = "Movie ready";
+
+        }
+        else
+        {
+            statusText.text = "No suitable movie found";
+            movieLoaded = false;
+        }
+    }
+}
